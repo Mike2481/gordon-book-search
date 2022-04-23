24b54351b5ab8d0a809f49b47a9089e48dcad9d8
Mike Gordon <vprmatrix55@gmail.com>
2022-04-23 10:56:43 -0700

    updated server schema index.js, connection.js, and refactored client/src/App.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/gordon-book-search', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
