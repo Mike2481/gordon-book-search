0413cd838dd3a4e296ae4d19da122d84708ee5d2
Mike Gordon <vprmatrix55@gmail.com>
2022-04-23 11:39:19 -0700

    added front end files

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,42 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($id: ID!) {
+      saveBook(bookId: $id) {
+        bookId: google book API
+        authors: []
+        description: String
+        title: String
+        image: Img
+        link: String
+      }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+
+`;
